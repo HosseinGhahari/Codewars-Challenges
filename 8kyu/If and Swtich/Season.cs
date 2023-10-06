ec013390d78f2d7ece5e42226e0f11ae9a3b53f7
HosseinGhahari <Hosseinsgh96@gmail.com>
2023-10-06 13:32:32 +0330

    Added codewars opposite-number challenge to github

---
+++ 8kyu/If and Swtich/Season.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_and_Swtich
+{
+    public enum Season
+    {
+        Summer,
+        Winter,
+    }
+}
---
+++ 8kyu/Opposite-Number/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+// My Solution : 
+
+
+public class Kata
+{
+    public static int Opposite(int number)
+    {
+        return number - (number * 2);
+    }
+}
+
+
+
+namespace Opposite_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Kata.Opposite(27));
+            Console.ReadKey();
+        }
+    }
+}

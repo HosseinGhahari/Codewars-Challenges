a9e57ce1390ee0cb518fbbaa77c98d9fb91cdc8f
HosseinGhahari <Hosseinsgh96@gmail.com>
2023-10-07 11:54:52 +0330

    Added codewars Sum-Arrays challenge to github

---
+++ 8kyu/Sum Arrays/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Write a function that takes an array of numbers and returns the sum of the numbers. 
+// The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+// Examples
+// Input: [1, 5.2, 4, 0, -1]
+// Output: 9.2
+
+// Input: []
+// Output: 0
+
+// Input: [-2.398]
+// Output: -2.398
+
+// Assumptions
+// You can assume that you are only given numbers.
+// You cannot assume the size of the array.
+// You can assume that you do get an array and if the array is empty, return 0.
+
+
+
+
+// My Solution --->
+
+
+public class Kata
+{
+    public static double SumArray(double[] array)
+    {
+
+        double sum = 0;
+        if(array.Length == 0)
+        {
+            return 0;
+        }
+
+        for(int i = 0; i < array.Length; i++)
+        {
+            sum += array[i];
+        }
+
+        return sum;
+
+
+    }
+
+}
+
+
+
+namespace Sum_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var result = Kata.SumArray(new double[] { 1, 5.2, 4, 0, -1 });
+            Console.WriteLine($"Sum of arrays is : {result}");
+            Console.ReadKey();
+
+
+        }
+    }
+}

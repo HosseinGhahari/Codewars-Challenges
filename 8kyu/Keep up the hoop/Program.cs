09ff38871ad0f3d9b92e09ddcee1a78e3b959170
HosseinGhahari <Hosseinsgh96@gmail.com>
2023-10-14 13:09:29 +0330

    Added codewars Keep-up-the-hoop challenge to github

---
+++ 8kyu/Keep up the hoop/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keep_up_the_hoop
+{
+
+    public class Kata
+    {
+        public static string HoopCount(int Number)
+        {
+
+            string WinMessage = "Great, now move on to tricks";
+            string TryAgainMessage = "Keep at it until you get it";
+            if (Number >= 10)
+            {
+                return WinMessage;
+            }
+            else
+            {
+                return TryAgainMessage;
+            }
+            
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           Console.WriteLine(Kata.HoopCount(10));
+           Console.ReadKey();
+
+        }
+    }
+}

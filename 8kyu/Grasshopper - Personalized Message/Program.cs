13af97d202ee64e893fe1e265712d97af532ad1e
HosseinGhahari <Hosseinsgh96@gmail.com>
2023-10-20 12:08:49 +0330

    Added codewars Grasshopper-Personalized-Message challenge to github

---
+++ 8kyu/Grasshopper - Personalized Message/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+//Create a function that gives a personalized greeting.
+//This function takes two parameters: name and owner.
+
+//Use conditionals to return the proper message:
+
+//case                       return
+//name equals owner         'Hello boss'
+//otherwise                 'Hello guest'
+
+
+
+// My Solution ------ >
+
+
+namespace Grasshopper___Personalized_Message
+{
+
+
+    public class Kata
+    {
+        public static string Greet(string name, string owner)
+        {
+            
+            if (name == owner)
+            {
+                return "Hello boss";
+            }
+            else
+            {
+                return "Hello guest";
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Kata.Greet("Jack", "Jack"));
+            Console.ReadKey();
+        }
+    }
+}

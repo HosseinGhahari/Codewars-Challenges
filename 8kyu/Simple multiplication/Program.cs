e04da28daf07829ea24b793b35990365402a81ff
HosseinGhahari <Hosseinsgh96@gmail.com>
2023-10-20 12:19:52 +0330

    Added codewars Simple-multiplication challenge to github

---
+++ 8kyu/Simple multiplication/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//This kata is about multiplying a given number by eight 
+//if it is an even number and by nine otherwise.
+
+
+
+// My Solution ------>
+
+
+namespace Simple_multiplication
+{
+
+    public class Multiplier
+    {
+        public static int Multiply(int x)
+        {
+           if (x % 2 == 0)
+           {
+                return x * 8;
+           }
+           else
+           {
+                return x * 9;
+           }
+
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Multiplier.Multiply(4));
+            Console.ReadKey();
+
+
+        }
+    }
+}

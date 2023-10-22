636b92151a5d2610abb48a5b70d20f480462045e
HosseinGhahari <Hosseinsgh96@gmail.com>
2023-10-22 12:34:54 +0330

    Added codewars Make-Upper-Case challenge to github

---
+++ 8kyu/MakeUpperCase/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+//Write a function which converts the input string to uppercase.
+
+
+// My Solution ------ > 
+
+public class Kata
+{
+    public static string MakeUpperCase(string str)
+    {
+       return str.ToUpper();
+    }
+
+}
+
+
+
+namespace MakeUpperCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Kata.MakeUpperCase("hello")); 
+            Console.ReadKey();
+
+        }
+    }
+}

f77b59a7f492831429d5096da5b5f49bf4281989
HosseinGhahari <Hosseinsgh96@gmail.com>
2023-10-23 11:25:44 +0330

    Added codewars Find-the-smallest-integer-in-the-array challenge to github

---
+++ 8kyu/Find the smallest integer in the array/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+// Given an array of integers your solution 
+// should find the smallest integer.
+
+// For example:
+
+// Given[34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+
+
+// My Solution -------- >
+
+
+namespace Find_the_smallest_integer_in_the_array
+{
+
+    public class Kata
+    {
+        public static int FindSmallestInt(int[] args)
+        {
+
+            // Solution 1 
+
+            int Holder = args[0];
+            for (int i = 0; i < args.Length; i++)
+            { 
+                if (args[i] < Holder)
+                {
+                   Holder = args[i];
+                }
+            }
+
+            return Holder;
+
+
+            /////////////////////////////////////////////
+
+
+            // Solution 2 
+
+            // return args.Min();
+
+
+            /////////////////////////////////////////////
+
+
+            // Solution 3 
+
+            // int Holder = args.Max();
+            // for (int i = 0; i < args.Length; i++)
+            // {
+            //     if (args[i] < Holder)
+            //     {
+            //         Holder = args[i];
+            //     }
+            // }
+
+            // return Holder;
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Kata.FindSmallestInt(new int[] { 78, 56, 232, 12, 11, 43 }));
+            Console.ReadKey();
+        }
+    }
+}

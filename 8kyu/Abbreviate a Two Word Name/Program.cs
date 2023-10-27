308363189c69e90884e2305a7819170912dabd1a
HosseinGhahari <Hosseinsgh96@gmail.com>
2023-10-27 12:36:24 +0330

    Added codewars Abbreviate-a-Two-Word-Name challenge to github

---
+++ 8kyu/Abbreviate a Two Word Name/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Write a function to convert a name into initials.
+// This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+
+
+// My Solution ------->
+
+
+
+namespace Abbreviate_a_Two_Word_Name
+{
+
+    public class Kata
+    {
+        public static string AbbrevName(string name)
+        {
+            string[] arr = name.Split(' ');
+            string Result = "";
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                Result += arr[0].Substring(0,1);
+                Result += ".";
+                Result += arr[1].Substring(0,1);
+            }
+
+            return Result.ToUpper();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.AbbrevName("sam fisher"));
+            Console.ReadKey();
+
+        }
+    }
+}

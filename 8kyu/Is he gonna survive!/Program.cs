2270f39e763af3f946e3ec4f692a6be373e0a030
HosseinGhahari <Hosseinsgh96@gmail.com>
2023-10-28 12:19:23 +0330

    Added codewars Is-he-gonna-survive! challenge to github

---
+++ 8kyu/Is he gonna survive!/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// A hero is on his way to the castle to complete 
+// his mission. However, he's been told that the 
+// castle is surrounded with a couple of powerful
+// dragons! each dragon takes 2 bullets to be defeated,
+// our hero has no idea how many bullets he should carry.. 
+// Assuming he's gonna grab a specific given number of bullets 
+// and move forward to fight another specific given number of 
+// dragons, will he survive?
+
+// Return true if yes, false otherwise :)
+
+
+
+
+// My Solution ------->>
+
+
+
+namespace Is_he_gonna_survive_
+{
+    class Kata
+    {
+        public static bool Hero(int bullets, int dragons)
+        {
+            if (bullets >= 2 * dragons)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Kata.Hero(100,40));
+            Console.ReadKey();
+
+        }
+    }
+}

1de84b87942548eee64f6ae8b49aecb89fcf8ab1
HosseinGhahari <Hosseinsgh96@gmail.com>
2023-10-28 11:25:58 +0330

    Added codewars Volume-of-a-Cuboid challenge to github

---
+++ 8kyu/Volume of a Cuboid/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Bob needs a fast way to calculate the volume of a 
+// cuboid with three values: the length, width and 
+// height of the cuboid. Write a function to help 
+// Bob with this calculation.
+
+
+
+// My Solution ------>
+
+namespace Volume_of_a_Cuboid
+{
+
+    public class Kata
+    {
+        public static double GetVolumeOfCuboid(double length, double width, double height)
+        {
+            return length * width * height;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.GetVolumeOfCuboid(2,5,6));
+            Console.ReadKey();
+        }
+    }
+}

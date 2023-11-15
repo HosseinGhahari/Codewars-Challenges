bf63cfe7a97eda8dbfe9531f53674b66f3117ccd
HosseinGhahari <Hosseinsgh96@gmail.com>
2023-11-15 15:58:24 +0330

    Added codewars Is-n-divisible-by-x-and-y? challenge to github

---
+++ 8kyu/Is n divisible by x and y!/Is n divisible by x and y!/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Create a function that checks if a number n is
+// divisible by two numbers x AND y. All inputs are 
+// positive, non-zero numbers.
+
+// Examples:
+
+// 1) n = 3, x = 1, y = 3 =>  true because   3 is divisible by 1 and 3
+// 2) n = 12, x = 2, y = 6 =>  true because  12 is divisible by 2 and 6
+// 3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
+// 4) n = 12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
+
+
+
+
+// My Solution ----------->
+
+
+namespace Is_n_divisible_by_x_and_y_
+{
+    public class DivisibleNb
+    {
+        public static bool IsDivisible(int n, int x, int y)
+        {
+            if (n % x == 0 && n % y == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DivisibleNb.IsDivisible(12,4,3));
+            Console.ReadKey();
+        }
+    }
+}

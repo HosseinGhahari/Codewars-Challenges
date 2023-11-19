43b05378bcb1a1cd2c441cb497fd5c7780a5a5b7
HosseinGhahari <Hosseinsgh96@gmail.com>
2023-11-19 12:17:30 +0330

    Added codewars Shortest-Word challenge to github

---
+++ 7kyu/Shortest Word/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Simple, given a string of words, 
+// return the length of the shortest word(s).
+
+// String will never be empty and you do not need 
+// to account for different data types.
+
+
+
+// My Solution ---------- >
+
+
+namespace Shortest_Word
+{
+    public class Kata
+    {
+        public static int FindShort(string s)
+        {
+
+            string[] list = s.Split(' ');
+            int count = list[0].Length;
+            for (int i = 1; i <= list.Length - 1; i++)
+            {
+                if (list[i].Length < count)
+                {
+                    count = list[i].Length;
+                }
+            }
+
+            return count;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.FindShort("bitcoin take over the world maybe who knows perhaps"));
+            Console.ReadKey();
+        }
+    }
+}

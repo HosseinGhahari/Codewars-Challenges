7442cff8b3dd5c4da5919e71f389b602c0df0c2b
HosseinGhahari <Hosseinsgh96@gmail.com>
2023-11-21 18:49:45 +0330

    Added codewars Exes-and-Ohs challenge to github

---
+++ 7kyu/Exes and Ohs/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Check to see if a string has the same amount 
+// of 'x's and 'o's. The method must return a 
+// boolean and be case insensitive. The string 
+// can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+
+
+// My Soultions ----------------- >
+
+
+namespace Exes_and_Ohs
+{
+
+    public static class Kata
+    {
+        public static bool XO(string input)
+        {
+
+            // Solution 1 
+
+            int Xholder = 0;
+            int Oholder = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if ((input[i] == 'x' || input[i] == 'X')  || input[i] == 0)
+                {
+                    Xholder++;
+                }
+                else if ((input[i] == 'o' || input[i] == 'O') || input[i] == 0)
+                {
+                    Oholder++;
+                }
+            }
+
+            if (Xholder == Oholder)
+            {
+                return true;
+            }
+            return false;
+
+
+
+
+            // Solution 2 with Lambda Expressions
+
+            // return input.ToLower().Count(i => i == 'x') == input.ToLower().Count(i => i == 'o');
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.XO("Oo"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 7kyu/Exes and Ohs/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Exes and Ohs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Exes and Ohs")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b4fc0cd3-d69a-4739-9022-8e9b2cd0b389")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

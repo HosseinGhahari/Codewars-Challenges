bebfa3f5ed946457a7170708ed7a9a0509c9c4ba
HosseinGhahari <Hosseinsgh96@gmail.com>
2024-01-10 12:22:04 +0330

    Added codewars Grasshopper-Basic-Function-Fixer Project To Github

---
+++ 8kyu/Grasshopper - Basic Function Fixer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Fix the function
+
+// I created this function to add five to any number 
+// that was passed in to it and return the new value.
+// It doesn't throw any errors but it returns the wrong number.
+
+// Can you help me fix the function?
+
+
+
+
+// My Solution ------------------>
+
+
+namespace Grasshopper___Basic_Function_Fixer
+{
+
+    public class Kata
+    {
+        public static int AddFive(int num)
+        {
+            // given code
+
+            // int total = num + 5;
+            // return num;
+
+
+            // My Answer
+
+               int total = num + 5;
+               return total;
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.AddFive(5));
+            Console.ReadKey();
+        }
+    }
+}

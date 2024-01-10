869b25bf46153766c86678feca7e6b4058e7f3ee
HosseinGhahari <Hosseinsgh96@gmail.com>
2024-01-10 12:23:15 +0330

    Added codewars Sum-Mixed-Array Project To Github

---
+++ 8kyu/Sum Mixed Array/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Given an array of integers as strings and numbers,
+// return the sum of the array values as if all were numbers.
+// Return your answer as a number.
+
+
+
+
+// My Solution ------------------>
+
+
+namespace Sum_Mixed_Array
+{
+    public class Kata
+    {
+        public static int SumMix(object[] x)
+        {
+            int total = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                total += Convert.ToInt32(x[i]);
+            }
+            return total;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.SumMix(new object[] { 9, 3, "7", "3" }));
+            Console.ReadKey();
+        }
+    }
+}
